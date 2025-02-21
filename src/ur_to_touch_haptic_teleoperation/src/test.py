2b9904e005d7cc49d4989236314e4dab0bf2ca00
adrien <adrien.mille42@gmail.com>
2025-02-21 14:42:41 +0000

    Ajout de la partie réseau

--- src/ur_to_touch_haptic_teleoperation/src/test.py
+++
@@ -1,6 +0,0 @@
-from optoforce import OptoForce22 as OptoForce
-from optoforce.status import no_errors
-
-with OptoForce(speed_hz=10, filter_hz=15, zero=False) as force_sensor:
-    while True:
-        print(force_sensor.read(only_latest_data=False))
