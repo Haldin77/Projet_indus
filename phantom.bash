84c6e8c92ebf3243a0142c5409cfd7f689af78ca
Projet-ECHO <dilhan.emir@etu.sorbonne-universite.fr>
2025-03-28 10:33:17 +0100

    Adaptation des fichiers .bash au PC

--- phantom.bash
+++ phantom.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #Configurer l'adresse ip
-sudo ifconfig wlp1s0 192.168.42.146 netmask 255.255.255.0 up
+sudo ifconfig wlp0s20f3 192.168.42.146 netmask 255.255.255.0 up
 
 sudo chmod 777 /dev/ttyACM0
 
 #Lancer le launchfile du phantom
 source /opt/ros/humble/setup.bash
-source ~/ros2_ws/install/setup.bash
+source install/setup.bash
 ros2 launch ur_to_touch_haptic_teleoperation phantom_launch.py 
