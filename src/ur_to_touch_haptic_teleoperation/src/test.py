ef2165e35d8cc36483e8de610541ef5384b25b8d
Dilhan EMIR <dilhan.emir@etu.sorbonne-universite.fr>
2025-04-04 21:18:22 +0200

    remise au propre main

--- src/ur_to_touch_haptic_teleoperation/src/test.py
+++
@@ -1,8 +0,0 @@
-from optoforce import OptoForce22 as OptoForce
-from optoforce.status import no_errors
-
-with OptoForce(speed_hz=1000, filter_hz=150, zero=True) as force_sensor:
-    while True:
-        print(force_sensor.read(only_latest_data=False))
-        
-    
